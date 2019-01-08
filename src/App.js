a1b29a95b964dd66e40e511d05a7ab05dce7342b
HarringtonR <ross.ludeke@gmail.com>
2019-01-08 14:35:52 -0600

    api config

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import { Text, View} from 'react-native';
 import reducers from './reducers';
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
-import config from '../config.js';
+import Config from '../config';
 
 class App extends Component {
+
   componentWillMount(){
+    console.log(Config.MY_KEY)
       const config = {
-    apiKey: "config.key",
+    apiKey: "Config.MY_KEY",
     authDomain: "manager-4fabc.firebaseapp.com",
     databaseURL: "https://manager-4fabc.firebaseio.com",
     projectId: "manager-4fabc",
@@ -22,6 +24,7 @@ class App extends Component {
   }
 
   render() {
+
     return (
       <Provider store={ createStore(reducers) }>
         <LoginForm />
