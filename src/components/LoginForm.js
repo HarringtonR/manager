5925d63dbd067e09455fc3ca09340fa21bcfa012
HarringtonR <ross.ludeke@gmail.com>
2019-01-09 01:55:54 -0600

    loginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import { Card, CardSection, Input, Button } from './common';
+import { connect } from 'react-redux';
+import { emailChanged } from '../actions';
 
 class LoginForm extends Component {
+  onEmailChange(text){
+    this.props.emailChanged(text);
+  }
+
   render() {
     return (
-        <Card>
+      <Card>
           <CardSection>
             <Input
               label= 'Email'
               placeholder='email@gmail.com'
+              onChangeText={() =>this.onEmailChange()}
+              value={this.props.email}
             />
           </CardSection>
           <CardSection>
@@ -17,15 +25,20 @@ class LoginForm extends Component {
               label='Password'
               placeholder='password'
             />
-            </CardSection>
-            <CardSection>
-              <Button>
-                Login
-              </Button>
-            </CardSection>
+          </CardSection>
+          <CardSection>
+            <Button>
+              Login
+            </Button>
+          </CardSection>
         </Card>
-      );
+    );
   }
 }
+const mapStateToProps = state => {
+  return {
+    email: state.auth.email
+  };
+};
 
-export default LoginForm;
+export default connect(mapStateToProps, { emailChanged })(LoginForm);
