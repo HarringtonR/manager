769b0c9aa9a501987652d7a7adf74edafa71b585
HarringtonR <ross.ludeke@gmail.com>
2019-02-09 12:12:13 -0600

    employeeCreate

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
+import EmplolyeeFormReducer from './EmployeeFormReducer';
 
 export default combineReducers({
-  auth: AuthReducer
+  auth: AuthReducer,
+  employeeForm: EmplolyeeFormReducer
 });
